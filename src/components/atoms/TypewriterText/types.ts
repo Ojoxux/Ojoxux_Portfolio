0cdc1e5befe3c35dd71e93dcd95d01b458596f3d
Jou Okuyama <ojoxux@gmail.com>
2025-02-02 00:08:28 +0900

    feat: ✨ TypewriterText コンポーネントの型定義を追加
    - StylePropsを継承した型インターフェースを作成
    - テキスト、速度、遅延、クラス名、完了コールバックのプロパティを定義

---
+++ src/components/atoms/TypewriterText/types.ts
@@ -0,0 +1,9 @@
+import { StyleProps } from '@/types/styles'
+
+export interface TypewriterTextProps extends StyleProps {
+  text: string
+  speed?: number
+  delay?: number
+  className?: string
+  onComplete?: () => void
+}
