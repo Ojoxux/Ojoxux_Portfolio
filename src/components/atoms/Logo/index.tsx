6eddc1042175336a442c016fb8040adc02f61b51
Jou Okuyama <ojoxux@gmail.com>
2025-02-01 21:33:51 +0900

    style: 🎨 Logoコンポーネントの幅を調整

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -3,7 +3,7 @@ import { LogoProps } from './types'
 export const Logo = ({ className }: LogoProps) => {
   return (
     <div
-      className={`flex items-center px-10 text-gray-400 border-r border-[#44475a] text-base font-medium ${className}`}
+      className={`flex items-center px-21 text-gray-400 border-r border-[#44475a] text-base font-medium ${className}`}
     >
       jou-okuyama
     </div>
