fecbeab370cb72691057aa75b7cd15cf732b7e6d
Jou Okuyama <ojoxux@gmail.com>
2025-02-01 20:14:02 +0900

    feat: ✨ ナビゲーションルート定数の追加
    - 静的なルート配列を定義
    - ホーム、アバウト、プロジェクト、コンタクトページのパスとラベルを設定

---
+++ src/utils/constants/navigation.ts
@@ -0,0 +1,18 @@
+export const routes = [
+  {
+    path: '/',
+    label: 'home',
+  },
+  {
+    path: '/about',
+    label: 'about-me',
+  },
+  {
+    path: '/projects',
+    label: 'projects',
+  },
+  {
+    path: '/contact-me',
+    label: 'contact-me',
+  },
+]
