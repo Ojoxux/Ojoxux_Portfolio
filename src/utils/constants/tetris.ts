272f2929392bad2b04d70f07e6565ab23f5f565a
Jou Okuyama <ojoxux@gmail.com>
2025-02-02 13:19:20 +0900

    feat: ✨ テトリスゲームの基本的な定数の定義

---
+++ src/utils/constants/tetris.ts
@@ -0,0 +1,64 @@
+export const BOARD_WIDTH = 10
+export const BOARD_HEIGHT = 20
+export const INITIAL_SPEED = 1000 // ミリ秒
+
+export const TETROMINOS = {
+  I: {
+    shape: [
+      [0, 0, 0, 0],
+      [1, 1, 1, 1],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+    color: '#00f0f0',
+  },
+  O: {
+    shape: [
+      [1, 1],
+      [1, 1],
+    ],
+    color: '#f0f000',
+  },
+  T: {
+    shape: [
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0],
+    ],
+    color: '#a000f0',
+  },
+  L: {
+    shape: [
+      [0, 0, 1],
+      [1, 1, 1],
+      [0, 0, 0],
+    ],
+    color: '#f0a000',
+  },
+  J: {
+    shape: [
+      [1, 0, 0],
+      [1, 1, 1],
+      [0, 0, 0],
+    ],
+    color: '#0000f0',
+  },
+  S: {
+    shape: [
+      [0, 1, 1],
+      [1, 1, 0],
+      [0, 0, 0],
+    ],
+    color: '#00f000',
+  },
+  Z: {
+    shape: [
+      [1, 1, 0],
+      [0, 1, 1],
+      [0, 0, 0],
+    ],
+    color: '#f00000',
+  },
+}
+
+export type TetrominoType = keyof typeof TETROMINOS
