147a6d5fe38454af9f487ec5825d84d6525c45d8
Jou Okuyama <ojoxux@gmail.com>
2025-02-14 14:31:52 +0900

    feat: 🧱 TetrisGame用の型定義を追加
    - TetrisGameコンポーネントの主要なインターフェースを定義
    - ゲーム画面、タイトル画面、スコアボード、コントロール情報の型を追加
    - GameStateからの型インポートを含む
    - 各インターフェースに必要なプロパティを設定

---
+++ src/components/molecules/TetrisGame/types.ts
@@ -0,0 +1,48 @@
+import { RefObject } from 'react'
+import { DraggableEventHandler } from 'react-draggable'
+import { GameState } from '@/hooks/useTetris'
+
+export interface TetrisGameProps {
+  className?: string
+  onScoreUpdate?: (score: number) => void
+  onGameOver?: () => void
+}
+
+export interface TitleScreenProps {
+  onStart: () => void
+  onClose?: () => void
+  className?: string
+}
+
+export interface GameScreenProps {
+  nodeRef: RefObject<HTMLDivElement>
+  position: { x: number; y: number }
+  onDrag: DraggableEventHandler
+  board: GameState['board']
+  currentPiece: GameState['currentPiece']
+  score: number
+  level: number
+  linesCleared: number
+  isGameOver: boolean
+  isPaused: boolean
+  nextPiece: GameState['nextPiece']
+  clearingLines: number[]
+  initGame: () => void
+  togglePause: () => void
+  onClose?: () => void
+  className?: string
+}
+
+export interface ScoreBoardProps {
+  score: number
+  level: number
+  linesCleared: number
+}
+
+export interface ControlsInfoProps {
+  className?: string
+}
+
+export interface NextPiecePreviewProps {
+  piece: GameState['nextPiece']
+}
