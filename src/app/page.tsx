67fdac9ab57834a84a94a95e167168eef7fb5f67
Jou Okuyama <j.okuyama.engineering@gmail.com>
2025-02-14 14:27:09 +0900

    feat: 🎮 ホームページにテトリスゲームを追加し、レイアウトを2カラムに更新
    - HomeHeroとTetrisGameを並列に配置
    - グリッドレイアウトを使用して左右に配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import { EditorLayout } from '@/components/templates/EditorLayout'
 import { HomeHero } from '@/components/organisms/HomeHero'
+import { TetrisGame } from '@/components/molecules/TetrisGame'
 
 export default function Home() {
   return (
     <EditorLayout>
-      <HomeHero />
+      <div className="grid grid-cols-2 gap-32 min-h-screen">
+        {/* 左側のHomeHero */}
+        <div className="flex items-center translate-y-[-5%]">
+          <HomeHero className="px-12" />
+        </div>
+
+        {/* 右側のTetrisGame */}
+        <div className="relative flex items-center justify-center translate-y-[3%]">
+          <TetrisGame className="-ml-[15%]" />
+        </div>
+      </div>
     </EditorLayout>
   )
 }
