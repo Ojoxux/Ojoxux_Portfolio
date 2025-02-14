8e8a6597b7740c9d58dffdd1ced001bf6200d16c
Jou Okuyama <j.okuyama.engineering@gmail.com>
2025-02-14 14:27:28 +0900

    chore: 🔤 Logoコンポーネントのテキストを大文字に変更

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -5,7 +5,7 @@ export const Logo = ({ className }: LogoProps) => {
     <div
       className={`flex items-center px-20 text-gray-400 border-r border-[#44475a] text-base font-medium ${className}`}
     >
-      jou-okuyama
+      Jou-Okuyama
     </div>
   )
 }
