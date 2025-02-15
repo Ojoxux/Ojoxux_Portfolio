b3d81cc61a968ec202c4e21e54147cc05e5e599e
Jou Okuyama <j.okuyama.engineering@gmail.com>
2025-02-15 20:26:53 +0900

    feat: ✨ AboutMeページコンポーネントを追加

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutMeContent from '@/components/organisms/AboutMeContent'
+
+const AboutMePage: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#44475a] p-8">
+      <AboutMeContent />
+    </div>
+  )
+}
+
+export default AboutMePage
