a0268e768bef1e5bbb622afc57affb6995889ef5
Jou Okuyama <j.okuyama.engineering@gmail.com>
2025-02-17 12:15:26 +0900

    feat: ✨ EditorLayout に BottomNav を追加し、レイアウトを調整

--- src/components/templates/EditorLayout/index.tsx
+++ src/components/templates/EditorLayout/index.tsx
@@ -1,4 +1,5 @@
 import { EditorNav } from '@/components/organisms/EditorNav'
+import { BottomNav } from '@/components/organisms/BottomNav'
 import { EditorLayoutProps } from './types'
 import { styles } from '@/utils/styles/constants'
 
@@ -6,9 +7,10 @@ export const EditorLayout = ({ children }: EditorLayoutProps) => {
   return (
     <div className="min-h-screen bg-[#282a36]">
       <EditorNav />
-      <main className={`min-h-[calc(100vh-44px)] ${styles.container} flex items-center`}>
+      <main className={`min-h-[calc(100vh-88px)] ${styles.container} flex items-center`}>
         {children}
       </main>
+      <BottomNav />
     </div>
   )
 }
