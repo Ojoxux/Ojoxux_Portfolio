0ded5074b0316f6c409394570e75083dce8edfb4
Jou Okuyama <j.okuyama.engineering@gmail.com>
2025-02-17 10:48:01 +0900

    refactor: ♻️ LineNumber インターフェースを別の型定義ファイルに分離

--- src/components/atoms/LineNumber/index.tsx
+++ src/components/atoms/LineNumber/index.tsx
@@ -1,6 +1,4 @@
-interface LineNumberProps {
-  number: number
-}
+import { LineNumberProps } from './types'
 
 export const LineNumber: React.FC<LineNumberProps> = ({ number }) => (
   <div className="h-6 text-sm leading-6">{String(number).padStart(2, '0')}</div>
