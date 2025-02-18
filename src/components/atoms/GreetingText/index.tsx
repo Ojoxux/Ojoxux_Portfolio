546a10069b6c48fc885c387d4411ff617411f35f
Jou Okuyama <ojoxux@gmail.com>
2025-02-18 11:19:17 +0900

    feat: ✨ GreetingTextコンポーネントの追加

---
+++ src/components/atoms/GreetingText/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TypewriterText } from '@/components/atoms/TypewriterText'
+import { GreetingTextProps } from './types'
+
+export const GreetingText = ({ text, onComplete }: GreetingTextProps) => {
+  return (
+    <div className="font-mono text-gray-200">
+      <TypewriterText text={text} delay={0} speed={50} onComplete={onComplete} />
+    </div>
+  )
+}
