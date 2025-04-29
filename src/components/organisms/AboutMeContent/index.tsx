1a3f4c9c520e476f6d0788372fcd69070de6bd5a
Jou Okuyama <ojoxux@gmail.com>
2025-04-29 18:29:02 +0900

    feat(organisms): ✨ AboutMeContentコンポーネントにコードハイライト機能を追加

--- src/components/organisms/AboutMeContent/index.tsx
+++ src/components/organisms/AboutMeContent/index.tsx
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { EditorTabs } from '@/components/molecules/EditorTabs'
 import { EditorContent } from '@/components/molecules/EditorContent'
 import { LineNumber } from '@/components/atoms/LineNumber'
 import { StatusBar } from '@/components/molecules/StatusBar'
 import { AboutMeContentProps } from './types'
+import { highlightCode } from '@/utils/shiki'
 
-const AboutMeContent: React.FC<AboutMeContentProps> = ({
+export const AboutMeContent: React.FC<AboutMeContentProps> = ({
   currentFile,
   openFiles,
   fileContent,
   onFileSelect,
   onCloseFile,
 }) => {
-  if (!fileContent) return null
+  const [highlightedHtml, setHighlightedHtml] = useState<string>('')
+  const [lineCount, setLineCount] = useState<number>(0)
 
-  const lines = fileContent.content.split('\n')
+  useEffect(() => {
+    if (fileContent?.content) {
+      let isMounted = true
+      const lang = fileContent.language || 'text'
+      highlightCode(fileContent.content, lang)
+        .then(html => {
+          if (isMounted) {
+            setHighlightedHtml(html)
+            setLineCount(fileContent.content.split('\n').length)
+          }
+        })
+        .catch(err => {
+          console.error('Failed to highlight code:', err)
+          if (isMounted) {
+            setHighlightedHtml(`<pre><code>${fileContent.content}</code></pre>`)
+            setLineCount(fileContent.content.split('\n').length)
+          }
+        })
+
+      return () => {
+        isMounted = false
+      }
+    } else {
+      setHighlightedHtml('')
+      setLineCount(0)
+    }
+  }, [fileContent])
+
+  if (!fileContent) return null
 
   return (
     <div className="h-full flex flex-col">
@@ -25,25 +55,19 @@ const AboutMeContent: React.FC<AboutMeContentProps> = ({
         onCloseFile={onCloseFile}
       />
 
-      <div className="flex-1 overflow-auto">
-        <div className="flex">
-          {/* 行番号 */}
-          <div className="w-[50px] flex-shrink-0 bg-[#282a36] text-[#6272a4] text-right pr-4 select-none border-r border-[#44475a]">
-            {lines.map((_, i) => (
-              <LineNumber key={i} number={i + 1} />
-            ))}
-          </div>
+      <div className="flex-1 flex overflow-y-auto py-2 relative">
+        <div className="w-[50px] flex-shrink-0 bg-[#282a36] text-[#6272a4] text-right pr-2 pl-2 select-none">
+          {[...Array(lineCount)].map((_, i) => (
+            <LineNumber key={i} number={i + 1} />
+          ))}
+        </div>
 
-          <EditorContent content={lines} />
-
-          {/* ミニマップ */}
-          <div className="w-[60px] flex-shrink-0 bg-[#282a36] border-l border-[#44475a]" />
+        <div className="flex-1 pl-4">
+          <EditorContent highlightedHtml={highlightedHtml} />
         </div>
       </div>
 
-      <StatusBar language={fileContent.language} />
+      <StatusBar language={fileContent.language || 'text'} />
     </div>
   )
 }
-
-export default AboutMeContent
