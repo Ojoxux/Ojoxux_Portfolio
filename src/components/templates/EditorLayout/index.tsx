0314419d31b32d10d4411a5a049881bdf0f0713f
Jou Okuyama <ojoxux@gmail.com>
2025-04-29 18:27:30 +0900

    style: 🎨 EditorLayoutコンポーネントのスタイルを更新し、余白を追加

--- src/components/templates/EditorLayout/index.tsx
+++ src/components/templates/EditorLayout/index.tsx
@@ -3,15 +3,12 @@
 import { EditorNav } from '@/components/organisms/EditorNav'
 import { BottomNav } from '@/components/organisms/BottomNav'
 import { EditorLayoutProps } from './types'
-import { styles } from '@/utils/styles/constants'
 
 export const EditorLayout = ({ children }: EditorLayoutProps) => {
   return (
     <div className="min-h-screen bg-[#282a36]">
       <EditorNav />
-      <main className={`min-h-[calc(100vh-88px)] ${styles.container} flex items-center`}>
-        {children}
-      </main>
+      <main className={`min-h-[calc(100vh-88px)] w-full px-10 flex items-center`}>{children}</main>
       <BottomNav
         onGithubClick={() => window.open('https://github.com/Ojoxux', '_blank')}
         onInstagramClick={() => window.open('https://instagram.com/ojoxux__18', '_blank')}
