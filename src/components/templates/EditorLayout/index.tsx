df639496689aae3fc999ad20f665ed5d48b6757b
Jou Okuyama <j.okuyama.engineering@gmail.com>
2025-06-07 23:51:37 +0900

    fix: EditorLayoutコンポーネントのメインセクションのパディングを変更

--- src/components/templates/EditorLayout/index.tsx
+++ src/components/templates/EditorLayout/index.tsx
@@ -8,7 +8,9 @@ export const EditorLayout = ({ children }: EditorLayoutProps) => {
   return (
     <div className='min-h-screen bg-[#282a36]'>
       <EditorNav />
-      <main className={'min-h-[calc(100vh-88px)] w-full px-10 flex items-center'}>{children}</main>
+      <main className={'min-h-[calc(100vh-88px)] w-full pl-0 pr-0 flex items-center'}>
+        {children}
+      </main>
       <BottomNav
         onGithubClick={() => window.open('https://github.com/Ojoxux', '_blank')}
         onInstagramClick={() => window.open('https://instagram.com/ojoxux__18', '_blank')}
